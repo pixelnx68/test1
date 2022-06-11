573d533712700041f0fdc7feb10f43bc7c806ea2
pixelnx68 <97878539+pixelnx68@users.noreply.github.com>
2022-06-11 11:07:26 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js by Vishal!</a>
+          Welcome to <a href="https://nextjs.org">Next.js by AMit!</a>
         </h1>
 
         <p className={styles.description}>
